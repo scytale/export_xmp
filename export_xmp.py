23ec99a78cf01d477340070152a229ffc5508541
scytale <huari4@gmail.com>
2018-08-08 09:13:03 -0700

    Add unicode handling

--- export_xmp.py
+++ export_xmp.py
@@ -21,8 +21,7 @@ def list_items(key,namespace):
 # namespace_string = str(namespace) if printed in output will display whole blob of xmp_dict. Need to pass the key from the calling function
     namespace_string = str(key)
     output = namespace_string + ": " + elements[0:][0] + " = " + elements[0:][1]
-# 2018-07-30T16:35:36-0700 Mon kept getting UnicodeEncodeError so let's ignore it and still get output
-    print(output.encode('ascii','ignore'))
+    print(output.encode('utf-8','replace'))
 
 # This function will list the namespace and call the list_items function to get the fields and values
 def list_dict(dict): 
@@ -36,9 +35,6 @@ def list_dict(dict):
 # main program
 myFiles = sys.argv[1:]
 
-# 2018-07-31T14:30:54-0700 Tue could use a file extension check for files with actual xmp embedded
-#file_extensions = ['.jpg','.gif','.png','.pdf','.psd','.eps','.tif']
-#file_extensions = ['.JPG','.gif','.png','.pdf','.psd','.eps','.ai','.jpeg']
 # add new fileypes as needed
 file_extensions = ['.jpg','.JPG','.gif','.png','.pdf','.psd','.eps','.tif','.ai','.jpeg']
 
@@ -54,15 +50,9 @@ for eachFile in myFiles:
   print("= Filename: " + eachFile + " =")
 
 # let's test for the file extension. If valid then display the xmp, else print "not a valid file type"
-# 2018-07-31T17:58:08-0700 Tue this only works if the extension is 4 chars long. What about .jpeg? 
-# this works only if assets are in the same directory as script else ../ and ./ will match the index -
-# input_file_extension  =  eachFile[eachFile.index('.'):] 
-# old way:  input_file_extension  =  eachFile[-4:]
-
 
   pos = eachFile.rfind('.',0,-1 )
   input_file_extension  =  eachFile[pos:]
-# input_file_extension = eachFile.endswith(file_extensions) and eachFile.rfind('.',0,-1)
   if input_file_extension in file_extensions:
     list_dict(xmp_dict)
   else:
